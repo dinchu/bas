a89bc086f6187fff226881eacc0ebc545396d213
pablo <pdelgado@harlemnext.com>
2022-03-23 00:38:50 +0100

    solve the first problem

--- execute.php
+++ execute.php
@@ -1,7 +1,7 @@
 <?php
 
 for($month = 1; $month<=12; $month++) {
-    var_dump(getLastWorkingDayInMonth($month, 2022));
+    var_dump(getLastWorkingDateInMonth($month, 2022)->format('Y-m-d'));
 }
 
 /*
@@ -20,19 +20,32 @@ The output of the utility should be a CSV file, containing the payment dates for
 For your convenience, we've added the following flow-chart to visualize the requirements
  */
 
-function getLastWorkingDayInMonth($month, $year) : DateTime {
+/**
+ * This method finds the last working day in a given month/year
+ * Using relative formats to modify the dates. read more here
+ * https://www.php.net/manual/en/datetime.formats.relative.php
+ * @param $month
+ * @param $year
+ * @return DateTime
+ */
+function getLastWorkingDateInMonth($month, $year) : DateTime {
     $currentMonth = new DateTime("{$year}-{$month}-01");
     $currentMonth->modify('last day of this month');
 
     if (isWeekend($currentMonth->getTimestamp())) {
         //process again to find the previous working day
-        var_dump('is weekend');
+        $currentMonth->modify('last friday of this month');
     }
 
     return $currentMonth;
 
 }
 
+/**
+ * Detects if the proposed date is weekend (Sun or Sat)
+ * @param int $timeStamp
+ * @return bool
+ */
 function isWeekend(int $timeStamp): bool {
     $weekDay = date('w', $timeStamp);
     return ($weekDay == 0 || $weekDay == 6);
