647fb4336551c332ac2685f06fc1054b40ba4db0
pablo <pdelgado@harlemnext.com>
2022-03-23 22:40:37 +0100

    improving the project

--- Makefile
+++ Makefile
@@ -6,16 +6,15 @@ help:	## Show this help
 	@fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/##//'
 	@echo ""
 .PHONY: help
+execute: ## execute app -a stdin
+	 docker run -it --rm -v ./:/usr/src/myapp --name basappInstance basapp
+.PHONY: execute
 
-down:	## Stop the docker containers for local development
-	docker-compose down
-.PHONY: down
-
-up:	## Start the docker containers for local development
+exec:	## Start the docker containers for local development
 	docker-compose up -d
 .PHONY: up
 
-build:	## Start the docker containers for local development
-	docker build -t basapp .docker
+build:	## build the docker container
+	docker build -t basapp .docker/
 .PHONY: build
 
--- execute.php
+++ execute.php
@@ -1,9 +1,9 @@
 <?php
 
-for($month = 1; $month<=12; $month++) {
-    var_dump(getLastWorkingDateInMonth($month, 2022)->format('Y-m-d'));
-}
 
+$a = readline('Enter a string: ');
+
+die('pepe');
 /*
 
 
@@ -15,10 +15,26 @@ The base salaries are paid on the last day of the month unless that day is a Sat
 On the 15th of every month bonuses are paid for the previous month, unless that day is a weekend.
 In that case, they are paid the first Wednesday after the 15th.
 The output of the utility should be a CSV file, containing the payment dates for the remainder of this year.
+
  The CSV file should contain a column for the month name, a column that contains the salary payment date for that month,
  and a column that contains the bonus payment date
 For your convenience, we've added the following flow-chart to visualize the requirements
  */
+
+
+for($month = 1; $month<=12; $month++) {
+
+    $salary   = getLastWorkingDateInMonth($month, 2022);
+    $bonus    = getWorkingDayInQuarter($month, 2022);
+    $output[] = [
+        $salary->format('M'),
+        $salary->format('D d'),
+        $bonus->format('D d')
+    ];
+
+}
+createCsv($output);
+var_dump($output);
 
 /**
  * This method finds the last working day in a given month/year
@@ -42,6 +58,23 @@ function getLastWorkingDateInMonth($month, $year) : DateTime {
 }
 
 /**
+ * This method returns a date with the closest working day to day 15th
+ * @param $month
+ * @param $year
+ * @return DateTime
+ */
+function getWorkingDayInQuarter($month, $year) : DateTime {
+    $currentMonth = new DateTime("{$year}-{$month}-15");
+
+    if (isWeekend($currentMonth->getTimestamp())) {
+        //process again to find the previous working day
+        $currentMonth->modify('wednesday next week');
+    }
+
+    return $currentMonth;
+}
+
+/**
  * Detects if the proposed date is weekend (Sun or Sat)
  * @param int $timeStamp
  * @return bool
@@ -51,6 +84,15 @@ function isWeekend(int $timeStamp): bool {
     return ($weekDay == 0 || $weekDay == 6);
 }
 
-function format(int $timeStamp): bool {
+function prepare(int $timeStamp): bool {
 
+}
+
+function createCsv(array $data): bool {
+    $fileName = 'output/salary.csv';
+    $csvHeader = [
+        'month', 'salary payment date', 'bonus payment date'
+    ];
+    $data = array_merge($csvHeader, $data);
+    fputcsv($fileName, $data);
 }
